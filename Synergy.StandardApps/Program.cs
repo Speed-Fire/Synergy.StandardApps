0614131a07a87dce31a4ab6623a311eaa5bc7c04
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-09-10 21:12:23 +0200

    Добавьте файлы проекта.

---
+++ Synergy.StandardApps/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.StandardApps
+{
+    public static class Program
+    {
+        static IHost AppHost;
+
+        [STAThread]
+        public static void Main(params string[] args)
+        {
+            var builder = Host.CreateDefaultBuilder(args);
+
+            builder.ConfigureServices(services =>
+            {
+                services.AddSingleton<App>();
+                services.AddSingleton<MainWindow>();
+            });
+
+            AppHost = builder.Build();
+
+            var app = AppHost.Services.GetRequiredService<App>();
+            app.Run();
+        }
+    }
+}
