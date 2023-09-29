713f9168274ef13c71a0df9a803407344559fea4
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-09-29 21:52:19 +0200

    Adding CalendarService interface.

---
+++ Synergy.StandardApps.Service/Calendar/ICalendarService.cs
@@ -0,0 +1,18 @@
+﻿using Synergy.StandardApps.Domain.Responses;
+using Synergy.StandardApps.EntityForms.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.StandardApps.Service.Calendar
+{
+    public interface ICalendarService
+    {
+        Task<IResponse<CalendarEventForm>> CreateEvent(CalendarEventCreationForm form);
+        Task<IResponse<CalendarEventForm>> UpdateEvent(CalendarEventCreationForm form, long id);
+        Task<IResponse<IEnumerable<CalendarEventForm>>> GetEvents(int month);
+        Task<IResponse<bool>> DeleteEvent(long id);
+    }
+}
