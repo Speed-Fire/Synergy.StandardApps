ee22032033051e49e851491fb84c418e3991a166
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-09-29 19:25:21 +0200

    Adding CalendarEvent forms.

---
+++ Synergy.StandardApps.EntityForms/Calendar/CalendarEventCreationForm.cs
@@ -0,0 +1,63 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Synergy.StandardApps.EntityForms.Calendar
+{
+    public class CalendarEventCreationForm : ObservableObject
+    {
+        private string title;
+        private string description;
+        private int color;
+
+        [Required]
+        [CustomValidation(typeof(CalendarEventCreationForm), "ValidateTitle")]
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+
+        public string Description
+        {
+            get => description;
+            set => SetProperty(ref description, value);
+        }
+
+        public Color Color
+        {
+            get
+            {
+                var r = (byte)( color & 255);
+                var g = (byte)((color & (255 << 8)) >> 8);
+                var b = (byte)((color & (255 << 16)) >> 16);
+
+                return Color.FromRgb(r, g, b);
+            }
+
+            set
+            {
+                var r = value.R;
+                var g = value.G;
+                var b = value.B;
+
+                var clr = r | (g << 8) | (b << 16);
+                SetProperty(ref color, clr);
+            }
+        }
+
+        public static ValidationResult ValidateTitle(string title, ValidationContext context)
+        {
+            if(string.IsNullOrWhiteSpace(title))
+            {
+                return new("Empty event name!");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
---
+++ Synergy.StandardApps.EntityForms/Calendar/CalendarEventForm.cs
@@ -0,0 +1,49 @@
+﻿using System.Windows.Media;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.StandardApps.EntityForms.Calendar
+{
+    public class CalendarEventForm
+    {
+        private long _id;
+        private string _title;
+        private string _description;
+        private int _color;
+
+        public long Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value;
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => _description = value;
+        }
+
+        public int Color
+        {
+            get => _color;
+            set => _color = value;
+        }
+
+        public Color GetColor()
+        {
+            var r = (byte)(_color & 255);
+            var g =  (byte)((_color & (255 << 8)) >> 8);
+            var b =  (byte)((_color & (255 << 16)) >> 16);
+
+            return System.Windows.Media.Color.FromRgb(r, g, b);
+        }
+    }
+}
