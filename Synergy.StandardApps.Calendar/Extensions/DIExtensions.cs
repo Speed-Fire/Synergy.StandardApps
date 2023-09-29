80a1892c42eabd1b223e954a697c38094c94e0ff
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-09-30 01:31:20 +0200

    Adding DIExtensions.

---
+++ Synergy.StandardApps.Calendar/Extensions/DIExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Synergy.StandardApps.Calendar.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.StandardApps.Calendar.Extensions
+{
+    public static class DIExtensions
+    {
+
+        public static IServiceCollection RegisterCalendarUI(this IServiceCollection services)
+        {
+            services
+                .AddTransient<CalendarVM>()
+                .AddTransient<CalendarPage>();
+
+            return services;
+        }
+    }
+}
