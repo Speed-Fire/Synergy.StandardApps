d03a26749f0c487f530061062a902891534fd172
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-09-30 02:24:07 +0200

    Fixing CalendarPage.

--- Synergy.StandardApps.Calendar/CalendarPage.xaml.cs
+++ Synergy.StandardApps.Calendar/CalendarPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using CommunityToolkit.Mvvm.Messaging;
 using Synergy.StandardApps.Calendar.Messages;
+using Synergy.StandardApps.Calendar.UserControls;
 using Synergy.StandardApps.Calendar.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -26,13 +27,15 @@ namespace Synergy.StandardApps.Calendar
         IRecipient<MonthLoadedMessage>
     {
         private readonly CalendarVM _vm;
+        private readonly List<CalendarDay> _cards;
 
-        private Thickness ItemMargin { get; }
+        public Thickness ItemMargin { get; }
 
         public CalendarPage(CalendarVM vm)
         {
             InitializeComponent();
             ItemMargin = new Thickness(15, 10, 15, 10);
+            _cards = new();
 
             WeakReferenceMessenger.Default
                 .RegisterAll(this);
@@ -51,7 +54,112 @@ namespace Synergy.StandardApps.Calendar
 
         private void LoadMonth()
         {
+            // remove all cards from grid
+            foreach (CalendarDay day in _cards)
+            {
+                CalendarGrid.Children.Remove(day);
+            }
+            _cards.Clear();
 
+
+            // getting events
+            var events = _vm.CalendarEvents;
+
+
+            // filling current month daycards
+            var dt = new DateTime(_vm.CurrentDate.Year,
+                                  _vm.CurrentDate.Month, 1);
+            var month = dt.Month;
+            var dtStart = dt;
+
+            var str = 0;
+            for(int i = DayOfWeekToOffset(dt.DayOfWeek);
+                dt.Month == month;
+                dt = dt.AddDays(1), i++, str = i % 7)
+            {
+                var ev = events.FirstOrDefault(e => e.Day == dt.Day);
+                var col = DayOfWeekToOffset(dt.DayOfWeek);
+
+                CalendarDay? card = null;
+
+                if(ev is null)
+                {
+                    card = new(dt.Day, dt.Month);
+                }
+                else
+                {
+                    card = new(ev);
+                }
+
+                card.SetValue(Grid.RowProperty, str + 1);
+                card.SetValue(Grid.RowProperty, col + 1);
+
+                _cards.Add(card);
+                CalendarGrid.Children.Add(card);
+            }
+
+
+            // adding blank daycard method
+            void AddBlankDay(DateTime _dt, int str)
+            {
+                var col = DayOfWeekToOffset(_dt.DayOfWeek);
+                var card = new CalendarDay(_dt.Day, _dt.Month, true);
+
+                card.SetValue(Grid.RowProperty, str);
+                card.SetValue(Grid.RowProperty, col + 1);
+
+                _cards.Add(card);
+                CalendarGrid.Children.Add(card);
+            }
+
+
+            // filling days before the month
+            if(dtStart.DayOfWeek != DayOfWeek.Monday)
+            {
+                do
+                {
+                    dtStart = dtStart.AddDays(-1);
+
+                    AddBlankDay(dtStart, 1);
+                }
+                while (dtStart.DayOfWeek != DayOfWeek.Monday);
+            }
+
+
+            // filling days after the month
+            if (dt.DayOfWeek != DayOfWeek.Sunday)
+            {
+                do
+                {
+                    dtStart = dtStart.AddDays(1);
+
+                    AddBlankDay(dtStart, str + 1);
+                }
+                while (dt.DayOfWeek != DayOfWeek.Sunday);
+            }
+        }
+
+        private int DayOfWeekToOffset(DayOfWeek dayOfWeek)
+        {
+            switch (dayOfWeek)
+            {
+                case DayOfWeek.Sunday:
+                    return 0;
+                case DayOfWeek.Monday:
+                    return 1;
+                case DayOfWeek.Tuesday:
+                    return 2;
+                case DayOfWeek.Wednesday:
+                    return 3;
+                case DayOfWeek.Thursday:
+                    return 4;
+                case DayOfWeek.Friday:
+                    return 5;
+                case DayOfWeek.Saturday:
+                    return 6;
+                default:
+                    return 0;
+            }
         }
     }
 }
