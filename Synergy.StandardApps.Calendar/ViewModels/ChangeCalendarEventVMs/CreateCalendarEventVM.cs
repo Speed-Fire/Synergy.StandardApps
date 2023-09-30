bea68c1f0d7981c903990332e8fb234a2e14b6dd
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-01 01:54:47 +0200

    Adding blank CalendarEvent creating and updating vms.

---
+++ Synergy.StandardApps.Calendar/ViewModels/ChangeCalendarEventVMs/CreateCalendarEventVM.cs
@@ -0,0 +1,30 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Synergy.StandardApps.Service.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Synergy.StandardApps.Calendar.ViewModels.ChangeCalendarEventVMs
+{
+    internal class CreateCalendarEventVM : ChangeCalendarEventVM
+    {
+        public CreateCalendarEventVM(ICalendarService calendarService, int day, int month)
+            : base(calendarService, day, month)
+        {
+        }
+
+        private AsyncRelayCommand? save;
+        public override ICommand Save => save ??
+            (save = new AsyncRelayCommand(SaveAsync));
+
+        public override ICommand Delete => throw new NotImplementedException();
+
+        private async Task SaveAsync()
+        {
+
+        }
+    }
+}
---
+++ Synergy.StandardApps.Calendar/ViewModels/ChangeCalendarEventVMs/UpdateCalendarEventVM.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.Input;
+using Synergy.StandardApps.EntityForms.Calendar;
+using Synergy.StandardApps.Service.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Synergy.StandardApps.Calendar.ViewModels.ChangeCalendarEventVMs
+{
+    internal class UpdateCalendarEventVM : ChangeCalendarEventVM
+    {
+        public UpdateCalendarEventVM(ICalendarService calendarService, CalendarEventForm form) 
+            : base(calendarService, form)
+        {
+        }
+
+        private AsyncRelayCommand? save;
+        public override ICommand Save => save ??
+            (save = new AsyncRelayCommand(SaveAsync));
+
+        public AsyncRelayCommand? delete;
+        public override ICommand Delete => delete ??
+            (delete = new AsyncRelayCommand(DeleteAsync));
+
+        private async Task SaveAsync()
+        {
+
+        }
+
+        private async Task DeleteAsync()
+        {
+
+        }
+    }
+}
