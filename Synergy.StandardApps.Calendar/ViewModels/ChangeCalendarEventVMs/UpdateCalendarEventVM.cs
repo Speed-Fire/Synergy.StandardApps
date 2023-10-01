bb54543fbcac8efd4f9e97a350774d1b305b026f
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-01 19:07:41 +0200

    Adding method implementations.

--- Synergy.StandardApps.Calendar/ViewModels/ChangeCalendarEventVMs/UpdateCalendarEventVM.cs
+++ Synergy.StandardApps.Calendar/ViewModels/ChangeCalendarEventVMs/UpdateCalendarEventVM.cs
@@ -1,6 +1,9 @@
 ﻿using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.Messaging;
+using Synergy.StandardApps.Calendar.Messages;
 using Synergy.StandardApps.EntityForms.Calendar;
 using Synergy.StandardApps.Service.Calendar;
+using Synergy.WPF.Common.Controls;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,12 +30,40 @@ namespace Synergy.StandardApps.Calendar.ViewModels.ChangeCalendarEventVMs
 
         private async Task SaveAsync()
         {
+            var res = await _calendarService.UpdateEvent(Form, _id);
 
+            if (res.StatusCode == Domain.Enums.StatusCode.Error)
+            {
+                await NotifyingGrid.ShowNotificationAsync("MainGrid",
+                        "Error", "Specified name is already taken!",
+                        System.Windows.MessageBoxButton.OK);
+
+                return;
+            }
+
+            WeakReferenceMessenger.Default
+                .Send(new CalendarEventUpdatedMessage(res.Data));
+            WeakReferenceMessenger.Default
+                .Send(new CloseCalendarEventChangingMessage(null));
         }
 
         private async Task DeleteAsync()
         {
+            var res = await _calendarService.DeleteEvent(_id);
 
+            if (res.StatusCode == Domain.Enums.StatusCode.Error)
+            {
+                await NotifyingGrid.ShowNotificationAsync("MainGrid",
+                        "Error", "Specified name is already taken!",
+                        System.Windows.MessageBoxButton.OK);
+
+                return;
+            }
+
+            WeakReferenceMessenger.Default
+                .Send(new CalendarEventDeletedMessage(Form.Day));
+            WeakReferenceMessenger.Default
+                .Send(new CloseCalendarEventChangingMessage(null));
         }
     }
 }
