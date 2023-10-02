bde78b43bffe220529069f5d4ea010bea8a3ce8f
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-02 15:32:52 +0200

    Adding OpenCalendarEventMessage.

---
+++ Synergy.StandardApps.Calendar/Messages/OpenCalendarEventMessage.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using Synergy.StandardApps.EntityForms.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.StandardApps.Calendar.Messages
+{
+    internal class OpenCalendarEventMessage : ValueChangedMessage<CalendarEventForm?>
+    {
+        public OpenCalendarEventMessage(CalendarEventForm? value) : base(value)
+        {
+        }
+    }
+}
