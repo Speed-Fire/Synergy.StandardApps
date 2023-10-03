7274f5a3280b3a6e1f4ec262302b70a3559b2fc2
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-03 19:25:55 +0200

    Adding BackgroundCalendarService.

---
+++ Synergy.StandardApps.Background/Workers/BackgroundCalendarService.cs
@@ -0,0 +1,38 @@
+﻿using CommunityToolkit.Mvvm.Messaging;
+using Microsoft.Extensions.Hosting;
+using Synergy.StandardApps.Background.Messages.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Synergy.StandardApps.Background.Workers
+{
+    public class BackgroundCalendarService
+        :
+        BackgroundService,
+        IRecipient<AddCalendarEventMessage>,
+        IRecipient<DeleteCalendarEventMessage>
+    {
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        #region Messages
+
+        void IRecipient<AddCalendarEventMessage>.Receive(AddCalendarEventMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        void IRecipient<DeleteCalendarEventMessage>.Receive(DeleteCalendarEventMessage message)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
