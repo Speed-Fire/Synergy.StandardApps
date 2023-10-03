56f777bdfacce9c0870d026a68724e973e627719
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-03 19:24:34 +0200

    Adding BackgroundCalendarService messages.

---
+++ Synergy.StandardApps.Background/Messages/Calendar/AddCalendarEventMessage.cs
@@ -0,0 +1,17 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using Synergy.StandardApps.Domain.Calendar;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.StandardApps.Background.Messages.Calendar
+{
+    public class AddCalendarEventMessage : ValueChangedMessage<CalendarEvent>
+    {
+        public AddCalendarEventMessage(CalendarEvent value) : base(value)
+        {
+        }
+    }
+}
---
+++ Synergy.StandardApps.Background/Messages/Calendar/DeleteCalendarEventMessage.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Synergy.StandardApps.Background.Messages.Calendar
+{
+    public class DeleteCalendarEventMessage : ValueChangedMessage<long>
+    {
+        public DeleteCalendarEventMessage(long value) : base(value)
+        {
+        }
+    }
+}
