ed7e94d91f9aad409cb4be56fc7023865066fce2
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-03 20:45:36 +0200

    Adding DI extensions for BackgroundCalendarService.

--- Synergy.StandardApps.Background/Extensions/DIExtensions.cs
+++ Synergy.StandardApps.Background/Extensions/DIExtensions.cs
@@ -2,6 +2,7 @@
 using Synergy.StandardApps.Background.Notifications;
 using Synergy.StandardApps.Background.Workers;
 using Synergy.StandardApps.Domain.Alarm;
+using Synergy.StandardApps.Domain.Calendar;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,10 +16,8 @@ namespace Synergy.StandardApps.Background.Extensions
         public static IServiceCollection RegisterBackground(this IServiceCollection services)
         {
             services
-                .AddTransient<INotifier<AlarmRecord>, AlarmToastNotifier>();
-
-            services
-                .RegisterAlarmBackground();
+                .RegisterAlarmBackground()
+                .RegisterCalendarBackground();
 
             return services;
         }
@@ -26,7 +25,17 @@ namespace Synergy.StandardApps.Background.Extensions
         public static IServiceCollection RegisterAlarmBackground(this IServiceCollection services)
         {
             services
-                .AddHostedService<BackgroundAlarmService>();
+                .AddHostedService<BackgroundAlarmService>()
+                .AddTransient<INotifier<AlarmRecord>, AlarmToastNotifier>();
+
+            return services;
+        }
+
+        public static IServiceCollection RegisterCalendarBackground(this IServiceCollection services)
+        {
+            services
+                .AddHostedService<BackgroundCalendarService>()
+                .AddTransient<INotifier<CalendarEvent>, CalendarEventToastNotifier>();
 
             return services;
         }
