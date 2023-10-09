9a7f452d2d8f9816536f9ef4916d3708f57c1414
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-09 20:39:55 +0200

    Fixing notes.

--- Synergy.StandardApps.Domain/Notes/Note.cs
+++ Synergy.StandardApps.Domain/Notes/Note.cs
@@ -13,6 +13,6 @@ namespace Synergy.StandardApps.Domain.Notes
         public string Name { get; set; } = "";
         public string Description { get; set; } = "";
         public DateTime Created { get; set; }
-        public DateTime? Updated { get; set; }
+        public DateTime Updated { get; set; }
     }
 }
