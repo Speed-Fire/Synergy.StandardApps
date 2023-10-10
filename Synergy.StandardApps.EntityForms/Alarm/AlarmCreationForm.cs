f6a9a1107647aececbf579be7fdf3c80590f9d96
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-10 23:24:28 +0200

    Adding week days properties.

--- Synergy.StandardApps.EntityForms/Alarm/AlarmCreationForm.cs
+++ Synergy.StandardApps.EntityForms/Alarm/AlarmCreationForm.cs
@@ -3,6 +3,7 @@ using Synergy.StandardApps.Domain.Alarm;
 using Synergy.StandardApps.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -40,6 +41,76 @@ namespace Synergy.StandardApps.EntityForms.Alarm
             get => dayMask;
             set => SetProperty(ref dayMask, value);
         }
+
+        #region Days
+
+        private bool _isInitRunning = false;
+
+        private bool monday;
+        public bool Monday
+        {
+            get => monday;
+            set => SetProperty(ref monday, value);
+        }
+
+        private bool tuesday;
+        public bool Tuesday
+        {
+            get => tuesday;
+            set => SetProperty(ref tuesday, value);
+        }
+
+        private bool wednesday;
+        public bool Wednesday
+        {
+            get => wednesday;
+            set => SetProperty(ref wednesday, value);
+        }
+
+        private bool thursday;
+        public bool Thursday
+        {
+            get => thursday;
+            set => SetProperty(ref thursday, value);
+        }
+
+        private bool friday;
+        public bool Friday
+        {
+            get => friday;
+            set => SetProperty(ref friday, value);
+        }
+
+        private bool saturday;
+        public bool Saturday
+        {
+            get => saturday;
+            set => SetProperty(ref saturday, value);
+        }
+
+        private bool sunday;
+        public bool Sunday
+        {
+            get => sunday;
+            set => SetProperty(ref sunday, value);
+        }
+
+        private void InitDays(WeekDay mask)
+        {
+            _isInitRunning = true;
+
+            Monday = AlarmHandler.IsAlarmedDay(mask, DayOfWeek.Monday);
+            Tuesday = AlarmHandler.IsAlarmedDay(mask, DayOfWeek.Tuesday);
+            Wednesday = AlarmHandler.IsAlarmedDay(mask, DayOfWeek.Wednesday);
+            Thursday = AlarmHandler.IsAlarmedDay(mask, DayOfWeek.Thursday);
+            Friday = AlarmHandler.IsAlarmedDay(mask, DayOfWeek.Friday);
+            Saturday = AlarmHandler.IsAlarmedDay(mask, DayOfWeek.Saturday);
+            Sunday = AlarmHandler.IsAlarmedDay(mask, DayOfWeek.Sunday);
+
+            _isInitRunning = false;
+        }
+
+        #endregion
 
         [Required]
         public AlarmSound Sound
@@ -81,6 +152,56 @@ namespace Synergy.StandardApps.EntityForms.Alarm
             }
 
             return ValidationResult.Success;
+        }
+
+        protected override void OnPropertyChanged(PropertyChangedEventArgs e)
+        {
+            base.OnPropertyChanged(e);
+
+            switch (e.PropertyName)
+            {
+                case nameof(DayMask):
+                    InitDays(DayMask);
+                    break;
+
+                case nameof(Monday):
+                    if (!_isInitRunning)
+                        SetDay(DayOfWeek.Monday);
+                    break;
+
+                case nameof(Tuesday):
+                    if (!_isInitRunning)
+                        SetDay(DayOfWeek.Tuesday);
+                    break;
+
+                case nameof(Wednesday):
+                    if (!_isInitRunning)
+                        SetDay(DayOfWeek.Wednesday);
+                    break;
+
+                case nameof(Thursday):
+                    if (!_isInitRunning)
+                        SetDay(DayOfWeek.Thursday);
+                    break;
+
+                case nameof(Friday):
+                    if (!_isInitRunning)
+                        SetDay(DayOfWeek.Friday);
+                    break;
+
+                case nameof(Saturday):
+                    if (!_isInitRunning)
+                        SetDay(DayOfWeek.Saturday);
+                    break;
+
+                case nameof(Sunday):
+                    if (!_isInitRunning)
+                        SetDay(DayOfWeek.Sunday);
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
