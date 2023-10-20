cdeea92cb8219f312f62625e3c9eadad0e1bd9f7
Vladislav Sidorovici <sidorvla@fit.cvut.cz>
2023-10-20 18:44:45 +0200

    Adding ExceptionTranslator.

---
+++ Synergy.StandardApps.Resources/ExceptionTranslator.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Synergy.StandardApps.Resources
+{
+	public class ExceptionTranslator
+	{
+		private static ExceptionTranslator _instance = null;
+		public static ExceptionTranslator Instance => _instance ??= new();
+		
+		private Dictionary<Type, string> _map;
+
+		private ExceptionTranslator()
+		{
+			_map = new();
+		}
+
+		public void Register<T>(string resourceKey) where T : Exception
+		{
+			if(_map.ContainsKey(typeof(T)))
+				throw new KeyIsTakenException();
+
+			_map.Add(typeof(T), resourceKey);
+		}
+
+		public void Unregister<T>() where T : Exception
+		{
+			_map.Remove(typeof(T));
+		}
+
+		public object GetValue<T>() where T : Exception
+		{
+			if(_map.TryGetValue(typeof(T), out var value))
+			{
+				var resource = Application.Current.TryFindResource(value)
+					?? throw new ResourceNotFoundException();
+
+				return resource;
+			}
+			else
+				throw new KeyNotFoundException(typeof(T).Name);
+		}
+	}
+
+	public class KeyIsTakenException : Exception
+	{
+		public KeyIsTakenException()
+			: base("Specified type of exception is already registered!")
+		{
+
+		}
+	}
+
+	public class ResourceNotFoundException : Exception
+	{
+		public ResourceNotFoundException()
+			: base("Specified resource name is not registered in the current application!")
+		{
+
+		}
+	}
+}
